893afaf843adc2d7d72f9dae2cfaabd51dd822c2
whl <48339784+whl-gh@users.noreply.github.com>
2019-03-22 23:12:54 +0800

    add react-redux react-router-dom prop-types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Home />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./home.scss";
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button className="btn">Button</button>
+            </div>
+        );
+    }
+}
+
+export default Home;
